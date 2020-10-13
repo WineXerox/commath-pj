4468fd9c65c2b263e4267924f7d9efb32cfaca41
WineXerox <tanapol.ta.61@ubu.ac.th>
2020-10-14 00:24:17 +0700

    fix

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -14,22 +14,22 @@ function Home() {
 
   return (
     <div>
-        <Button onClick={()=>{nextPage("BasicComputing")}} color="danger">
+        <Button onClick={()=>{nextPage("BasicComputing")}} color="success">
             <h5>1.Basic Computing</h5>
         </Button><br/>
-        <Button onClick={()=>{nextPage("LinearEquations")}} color="danger">
+        <Button onClick={()=>{nextPage("LinearEquations")}} color="success">
             <h5>2.Linear Equations</h5>
         </Button><br/>
-        <Button onClick={()=>{nextPage("Interpolation")}} color="danger">
+        <Button onClick={()=>{nextPage("Interpolation")}} color="success">
             <h5>3.Interpolation	</h5>
         </Button><br/>
-        <Button onClick={()=>{nextPage("Differentiation")}} color="danger">
+        <Button onClick={()=>{nextPage("Differentiation")}} color="success">
             <h5>4.Differentiation</h5>
         </Button><br/>
-        <Button onClick={()=>{nextPage("Integration")}} color="danger">
+        <Button onClick={()=>{nextPage("Integration")}} color="success">
             <h5>5.Integration	</h5>
         </Button><br/>
-        <Button onClick={()=>{nextPage("Rootfinding")}} color="danger">
+        <Button onClick={()=>{nextPage("Rootfinding")}} color="success">
             <h5>6.Root-finding</h5>
         </Button><br/>
         
